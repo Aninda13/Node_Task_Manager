4203b3227cfaabc1b9f8676ad6e5f7e9054f77b6
Aninda13 <Aninda13Desktop@users.noreply.github.com>
2020-07-27 01:57:54 -0400

    Initial Planning

---
+++ mongodb.js
@@ -0,0 +1,25 @@
+// curd : create read update delete
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient; // provies functions necessary
+// const ObjectID = mongodb.ObjectID;
+// Shorthand syntax and doing thise two things using destructuring
+const {MongoClient , ObjectID, ResumeToken } = require('mongodb');
+
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if(error){
+        console.log('Unable to connect to the Database')
+    }
+
+   const db =  client.db(databaseName)
+   
+   
+})
+
+
+
