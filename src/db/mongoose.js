02ca8e216ed6bde6c26472fb0718b343ddcbf087
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2020-08-27 03:13:57 -0400

    rest api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+// Mongoose just connects to the localhost now
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,82 +10,21 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required : true,
-        trim: true
-
-    },
-    age : {
-        type: Number,
-        min: [0, 'age must be positive'],
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password cant be password')
-            }
-        }
-    }
-})
 
 
 
-const myself = new User({
-    name: 'Chad3',
-    age : 13,
-    email: 'andy3@gmail.com',
-    password: 'somesword'
-})
-
-myself.save().then(() => {
-    console.log(myself)
-}).catch((error) => {
-    console.log('Error!', error)
-})
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
-const task = new Task({
-    description: '    Going back to nap'
-})
+// const task = new Task({
+//     description: '    Going back to nap'
+// })
 
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 
 
