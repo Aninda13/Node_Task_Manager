4c13eab1b0208df21e5ca3239364620b39a6ac19
Aninda13 <50640239+Aninda13@users.noreply.github.com>
2020-08-28 03:43:08 -0400

    refactored Routers

--- src/index.js
+++ src/index.js
@@ -1,154 +1,17 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/users')
-
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(userRouter)
+app.use(taskRouter)
 app.use(express.json())
 
-app.post('/users', async(req,res) => {
-    const user = new User(req.body)
-    
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }catch(e){
-        res.status(400).send(e)
-    }
-    
-    await user.save()
-
-
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((e) => {
-    //     res.status(400) //https status report 400 as user side error
-    //     res.send(e)
-    // })
-})
-
-app.get('/users', async (req,res) => {
-    try{
-        const users= await User.find({})
-        res.send(users)
-    }catch(e){
-        res.status(500).send(e)
-    }
-    
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send() // status 500 is internal server error
-    // })
-})
-
-// get by user id----> dynamic value generates(id can be any name here) :id
-app.get('/users/:id', async(req,res) => {
-    const _id = req.params.id
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch(e){
-        res.status(500).send(e)
-    }
-
-
-    // User.findById(_id).then((user) => {
-    //     if(!user){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e)=> {
-    //     res.status(500).send(e)
-    // })
-    
-   // console.log(req.params)
-})
-
-app.patch('/users/:id' ,async(req,res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-    if(!isValidOperation){
-        return res.status(400).send({ error: 'Invalid operation'})
-    }
-
-    try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-        
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-
-
-app.post('/tasks' ,async (req,res) => {
-    const task = new Task(req.body)
-
-    try{
-        await task.save()
-        res.send(task)
-    }catch(e){
-        res.status(400).send(e)
-    }
-    
-    
-    // task.save().then(() => {
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(400)
-    //     res.send(e)
-    // })
-})
-
-app.get('/tasks', async (req,res) => {
-    try{
-        const tasks = await Task.find({})
-        res.send(tasks)
-    }catch(e){
-        res.status(500).send(e)
-    }
-
-    // Task.find({}).then((task) => {
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send(e)
-    // })
-})
-
-
-app.get('/tasks/:id', async (req,res) => {
-    const _id = req.params.id
-
-    try{
-        const task = await Task.findById(_id)
-        res.send(task)
-    }catch(e){
-        res.status(404).send(e)
-    }
-
-    // Task.findById(_id).then((task) => {
-    //     if(!task){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send(e)
-    // })
-    
-})
 
 app.listen(port , () => {
     console.log('Server is up on port' + port)
---
+++ src/routers/user.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const User = require('../models/users')
+const router = new express.Router()
+
+router.get('/test', (req, res) => {
+    res.send('From a router file')
+})
+
+
+router.post('/users', async(req,res) => {
+    const user = new User(req.body)
+    
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+    
+    await user.save()
+
+
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((e) => {
+    //     res.status(400) //https status report 400 as user side error
+    //     res.send(e)
+    // })
+})
+
+router.get('/users', async (req,res) => {
+    try{
+        const users= await User.find({})
+        res.send(users)
+    }catch(e){
+        res.status(500).send(e)
+    }
+    
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send() // status 500 is internal server error
+    // })
+})
+
+// get by user id----> dynamic value generates(id can be any name here) :id
+router.get('/users/:id', async(req,res) => {
+    const _id = req.params.id
+    try{
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+
+
+    // User.findById(_id).then((user) => {
+    //     if(!user){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e)=> {
+    //     res.status(500).send(e)
+    // })
+    
+   // console.log(req.params)
+})
+
+router.patch('/users/:id' ,async(req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+    if(!isValidOperation){
+        return res.status(400).send({ error: 'Invalid operation'})
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+router.delete('/users/:id' ,async(req,res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
+module.exports = router
